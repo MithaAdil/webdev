c333a07f4ef3f52696ade091fb6c0a86dd47615e
MithaAdil <mitaadil12@gmail.com>
2022-04-11 22:01:43 +0800

    Belajar Bahasa Pemrograman1

---
+++ belajar-php/index.php
@@ -0,0 +1,20 @@
+<?php
+
+// $nama = "Madre";
+// echo "halo nama saya $nama";
+
+/*
+echo "<br>";
+
+// define("NAMA", "Madre Volenta");
+
+$nama = "Madre";
+echo "halo nama saya $nama";
+*/
+
+$nama = array("Madre", "Tasya", "Bli Dodi");
+
+echo "Halo nama saya ".$nama[0]." saya punya teman dengan nama ".$nama[1]. "
+Kami memiliki mentor yang bernama ".$nama[2];
+
+?>
